1d99e75c0d959cd66c3a908793a2c2b8d0df01b4
walumbe <nathanwalumbe@gmail.com>
2023-02-25 13:36:47 +0300

    retrieve chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -16,7 +16,9 @@ class ChirpController extends Controller
     public function index():View
     {
         // return response("Hello Jonathan");
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get()
+        ]);
     }
 
     /**
